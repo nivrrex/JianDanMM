5476506eeb21612205a5366f8f49eedc8f3834c2
nivrrex <nivrrex@gmail.com>
2015-06-18 20:35:46 +0800

    modified:   README.md 	new file:   jiandan.mm.rb

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# JianDanMM
+﻿# About JianDan MM
+
 Get jandan.net ooxx picture.
+获取煎蛋网妹子图的图片，并保存在本地。
+
+
+## 更新
+2013/9/10 V 1.0，直接使用net/http标准库进行抓取
+2015/6/17 V 2.0，由于煎蛋妹妹改版后，单纯抓页面会出现403错误。所以使用watir，模拟浏览器进行抓取
+2015/6/17 V 3.0，发现watir有时会Timeout error，重新抓包后发现，添加User-Agent的head标识即可正常返回200 OK。参考http://augustl.com/blog/2010/ruby_net_http_cheat_sheet
+
+## 备注
+脚本执行目录需手动新建test文件夹
---
+++ jiandan.mm.rb
@@ -0,0 +1,113 @@
+#encoding:gbk
+require 'net/http'
+
+def get_file_name(url)
+    re = /http.*\/(.*)/.match(url)
+    if re != nil
+        return re[1]
+    else
+        return nil
+    end
+end
+
+def get_file_name2(url)
+    re = url.gsub("https://","")
+    re = re.gsub("http://","")
+    re = re.gsub("/","-")
+    re = re.gsub("?","")
+    if re != nil
+        return re
+    else
+        return nil
+    end
+end
+
+def get_jpg(url)
+    begin
+        redirect = 1
+        redirecting = false 
+        begin
+            uri = URI.parse(url) 
+            req = Net::HTTP::Get.new(uri.path) 
+            res = Net::HTTP.start(uri.host, uri.port) do |http| 
+                http.read_timeout = 10
+                http.request(req) 
+            end 
+            if res.header['location'] # 遇到重定向，则url设定为location，再次抓取 
+                url = res.header['location']  
+                redirecting = true 
+            end 
+            redirect -= 1 
+        end while redirecting and redirect>=0 
+    rescue Timeout::Error
+        puts "****** #{url} is not get."
+        return
+    rescue Errno::ETIMEDOUT
+        puts "****** #{url} is not open."
+        return
+    rescue Errno::ECONNRESET
+        puts "****** #{url} is 'An existing connection was forcibly closed by the remote host.'."
+        return
+    rescue Errno::EADDRNOTAVAIL
+        puts "****** is 'The requested address is not valid in its context.'."
+        return
+    rescue ArgumentError
+        puts "****** #{url} is 'HTTP request path is empty'."
+        return
+    rescue SocketError
+        puts "****** #{url} is 不知道这样的主机。."
+        return
+    rescue EOFError
+        puts "****** is end of file reached."
+        return
+    rescue Errno::ECONNREFUSED
+        puts "****** is 'No connection could be made because the target machine actively refused it'."
+        return
+    rescue URI::InvalidURIError
+        puts "****** is bad URI."
+        return
+    rescue
+        puts "****** is other error."
+        return
+    end
+ 
+    filename = get_file_name2(url)
+ 
+    begin
+        open("./test/" + filename, "wb") do |file|
+            file.write(res.body)
+        end
+    rescue
+        puts "++++++ is write error."
+        return
+    end
+ 
+    puts "Write OK ... #{filename}"
+end
+
+1001.upto (1430) do |i|
+    uri = URI.parse('http://jandan.net/ooxx/page-' + i.to_s())  
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Get.new(uri.request_uri)
+    request.initialize_http_header({"User-Agent" => "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko"})
+    
+    response = http.request(request)
+    head = response.code
+    html = response.body
+        
+    puts
+    puts "reading " + 'http://jandan.net/ooxx/page-' + i.to_s() + " *** Return Code " + head
+    puts
+
+    html.scan(/<p><img src="(.*?)" \/>/) do |data|
+        data.each do |file_path|
+            puts data
+            STDOUT.flush
+            get_jpg (file_path)
+            STDOUT.flush
+            sleep Random.rand(0.3..0.5)
+        end
+    end
+    sleep Random.rand(0.8..1.0)
+end
+
