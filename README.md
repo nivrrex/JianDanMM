ec9cb73a2806a6bdb7bd4a7e55181e2f2dc8d7f1
nivrrex <nivrrex@gmail.com>
2016-10-15 22:33:14 +0800

    修改：     .gitignore 	修改：     README.md 	修改：     config.json 	修改：     jiandan.mm.go 	修改：     jiandan.mm.py 	修改：     jiandan.mm.rb

--- README.md
+++ README.md
@@ -6,29 +6,33 @@ Get jandan.net ooxx picture.
 
 
 ## 更新
-2013/9/10  V1.0  直接使用net/http标准库进行抓取。
+2013/9/10   V1.0   直接使用net/http标准库进行抓取。
 
-2015/6/17  V2.0  由于煎蛋妹妹改版后，单纯抓页面会出现403错误。所以使用watir，模拟浏览器进行抓取。
+2015/6/17   V2.0   由于煎蛋妹妹改版后，单纯抓页面会出现403错误。所以使用watir，模拟浏览器进行抓取。
 
-2015/6/17  V2.1  发现watir有时会Timeout error，重新抓包后发现，添加User-Agent的head标识即可正常返回200 OK，重新使用net/http实现。
+2015/6/17   V2.1   发现watir有时会Timeout error，重新抓包后发现，添加User-Agent的head标识即可正常返回200 OK，重新使用net/http实现。
 
 参考https://github.com/augustl/net-http-cheat-sheet
 
-2015/6/19  V2.2  增加记录图片的支持和反对记录数据log，同时完善对gif图片的提取
+2015/6/19   V2.2   增加记录图片的支持和反对记录数据log，同时完善对gif图片的提取
 
-2015/6/19  V2.3  根据支持和反对的比例，对保存的文件名添加“very\high\good\normal\bad”标识符
+2015/6/19   V2.3   根据支持和反对的比例，对保存的文件名添加“very\high\good\normal\bad”标识符
 
-2015/7/23  V3.0  增加Golang语言实现，同时完美支持jpg和gif文件，前期Ruby版本的gif文件下载的是缩略图
+2015/7/23   V3.0   增加Golang语言实现，同时完美支持jpg和gif文件，前期Ruby版本的gif文件下载的是缩略图
 
-2015/7/23  V3.1  jandan.net对妹子图的抓取，每隔50个页面会重置，此时403错误。通过变更User-Agent可以解决该问题
+2015/7/23   V3.1   jandan.net对妹子图的抓取，每隔50个页面会重置，此时403错误。通过变更User-Agent可以解决该问题
 
-2015/7/24  V3.2  根据Golang实现，修改Ruby实现，目前已经支持正常Gif抓取
+2015/7/24   V3.2   根据Golang实现，修改Ruby实现，目前已经支持正常Gif抓取
 
-2015/7/28  V3.3  Golang实现部分优化，同时可以通过config.json设置开始及结束的抓取页面,避免反复编译
+2015/7/28   V3.3   Golang实现部分优化，同时可以通过config.json设置开始及结束的抓取页面,避免反复编译
 
-2015/8/26  V3.4  对Golang实现的部分错误处理进行完善
+2015/8/26   V3.4   对Golang实现的部分错误处理进行完善
 
-2016/4/2  V4.0  增加Python语言实现，使用requests库，实现确实很简单。目前发现如果一个回复中，有多个图片的，可能存在遗漏，待更新。
+2016/4/2    V4.0   增加Python语言实现，使用requests库，实现确实很简单。目前发现如果一个回复中，有多个图片的，可能存在遗漏，待更新。
+
+2016/10/15  V5.0   Python语言实现，改用pyquery(lxml)库，同时实现一个回复中多个图片的下载。（需在脚本目录下创建test文件夹）
+
+Windows系统可以在 http://www.lfd.uci.edu/~gohlke/pythonlibs/#lxml 下载lxml库
 
 ## 备注
 ~~脚本执行目录需手动新建test文件夹，~~目前文件下载到脚本当前目录
--- jiandan.mm.py
+++ jiandan.mm.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 import requests
-import re
+import re,string
 import json
 import time
+
+from pyquery import PyQuery as pyq
+from lxml import etree
 
 def get_file_name(url):
     pattern = re.compile(r'http.*//(.*)')
@@ -36,7 +39,7 @@ def get_scale(scale):
     return level
 
 
-def get_jpg(url , support , unsupport):
+def get_jpg(id, url, support, unsupport):
     scale = float(support) / float(unsupport)
     rank = get_scale(scale)
 
@@ -69,62 +72,123 @@ def get_jpg(url , support , unsupport):
     finally:
         f.close()
 
-    print("Write OK ... {0}".format(filename))
+    print("Write OK ... {0}   {1}\t{2}\t{3}".format(id,filename,support,unsupport))
+
 
 def get_jiandan_mm_pic(page_num):
     url = 'http://jandan.net/ooxx/page-' + str(page_num)
-    r = requests.get(url)
-    pic_body = r.text
-    return_status_code = r.status_code
-    print('reading ...  http://jandan.net/ooxx/page-{0}   :   Return Code -- {1} . \n'.format(page_num,return_status_code))
-    #print(pic_body.encode("utf-8"))
+    html = pyq(url)
+    #print(html)
 
     hash_pic_message = {}
+    for element in html('li div div.row div.text'):
+        img = pyq(element)('img')
+        if img != None:
+            id = pyq(element)('span a').text()
+            id = id.replace("vote-","")
+            hash_pic_message[id]={}
+            hash_pic_message[id]['ID']=id
+            hash_pic_message[id]['URL']=[]
+            hash_pic_message[id]['FileName']=[]
 
-    reg_str_jpg = 'comment-(\d*?)">\d*?</a></span><p>.*?\s*<img src="([a-zA-z]+://[^\s]+?)" /></p>'
-    pattern_jpg = re.compile(reg_str_jpg)
-    result_jpg = pattern_jpg.findall(pic_body)
-    for tdata_jpg in result_jpg:
-        if not tdata_jpg[0] in hash_pic_message:
-            hash_pic_message[tdata_jpg[0]]={}
-            hash_pic_message[tdata_jpg[0]]['URL']=tdata_jpg[1]
-            hash_pic_message[tdata_jpg[0]]['FileName']=get_file_name2(tdata_jpg[1])
-            #print(tdata_jpg)
-        else:
-            print("...error... already has this picture. {0}".format(tdata_jpg))
+            if img.attr('org_src') == None:
+                for t in img:
+                    url = img(t).attr('src')
+                    hash_pic_message[id]['URL'].append(url)
+                    hash_pic_message[id]['FileName'].append(get_file_name2(url))
+    #            print(pyq(vvv)('img').attr('src'))
+            else:
+                for t in img:
+                    url = img(t).attr('org_src')
+                    hash_pic_message[id]['URL'].append(url)
+                    hash_pic_message[id]['FileName'].append(get_file_name2(url))
+    #            print(pyq(vvv)('img').attr('org_src'))
 
-    reg_str_gif = 'comment-(\d*?)">\d*?</a></span><p>.*?\s*<img src="[a-zA-z]+://[^\s]+?" org_src="([a-zA-z]+://[^\s]+?)" o'
-    pattern_gif = re.compile(reg_str_gif)
-    result_gif = pattern_gif.findall(pic_body)
-    for tdata_gif in result_gif:
-        if not tdata_gif[0] in hash_pic_message:
-            hash_pic_message[tdata_gif[0]]={}
-            hash_pic_message[tdata_gif[0]]['URL']=tdata_gif[1]
-            hash_pic_message[tdata_gif[0]]['FileName']=get_file_name2(tdata_gif[1])
-            #print(tdata_gif)
-        else:
-            print("...error... already has this picture. {0}".format(tdata_gif))
+    for element in html('li div div.row div.vote'):
+        id = pyq(element).attr('id')
+        id = id.replace("vote-","")
+        
+        vote = pyq(element).text()
+        #hash_pic_message[id]['vote'] = vote
 
-    reg_str_vote = '<div class="vote" id="vote-(\d*?)">.*?<span id="cos_support-\d*?">(\d*?)<\/span>.*?<span id="cos_unsupport-\d*?">(\d*?)<\/span>'
-    pattern_vote = re.compile(reg_str_vote)
-    result_vote = pattern_vote.findall(pic_body)
-    for tdata_vote in result_vote:
-        if tdata_vote[0] in hash_pic_message:
-            support = tdata_vote[1]
-            unsupport = tdata_vote[2]
+        reg_vote = 'OO \[ (\d.*) \] XX \[ (\d.*) \]'
+        pattern = re.compile(reg_vote)
+        result = pattern.findall(vote)
+        if result != None:
+            support = result[0][0]
+            unsupport = result[0][1]
+            hash_pic_message[id]["Support"] = support
+            hash_pic_message[id]["UnSupport"] = unsupport
+            
             scale =  float(support) / float(unsupport)
             rank = get_scale(scale)
-            hash_pic_message[tdata_vote[0]]["Support"] = support
-            hash_pic_message[tdata_vote[0]]["UnSupport"] = unsupport
-            hash_pic_message[tdata_vote[0]]["Scale"] = scale
-            hash_pic_message[tdata_vote[0]]["Rank"] = rank
-        else:
-            print("...error... no already has this picture. {0}".format(tdata_vote))
-
+            hash_pic_message[id]["Scale"] = scale
+            hash_pic_message[id]["Rank"] = rank
+            
     for value in hash_pic_message.values():
         #print(value)
         pass
     return hash_pic_message.values()
+
+
+
+#################################OLD#################################
+#def get_jiandan_mm_pic(page_num):
+#    url = 'http://jandan.net/ooxx/page-' + str(page_num)
+#    r = requests.get(url)
+#    pic_body = r.text
+#    return_status_code = r.status_code
+#    print('reading ...  http://jandan.net/ooxx/page-{0}   :   Return Code -- {1} . \n'.format(page_num,return_status_code))
+#    #print(pic_body.encode("utf-8"))
+#
+#    hash_pic_message = {}
+#
+#    reg_str_jpg = 'comment-(\d*?)">\d*?</a></span><p>.*?\s*<img src="([a-zA-z]+://[^\s]+?)" /></p>'
+#    pattern_jpg = re.compile(reg_str_jpg)
+#    result_jpg = pattern_jpg.findall(pic_body)
+#    for tdata_jpg in result_jpg:
+#        if not tdata_jpg[0] in hash_pic_message:
+#            hash_pic_message[tdata_jpg[0]]={}
+#            hash_pic_message[tdata_jpg[0]]['URL']=tdata_jpg[1]
+#            hash_pic_message[tdata_jpg[0]]['FileName']=get_file_name2(tdata_jpg[1])
+#            #print(tdata_jpg)
+#        else:
+#            print("...error... already has this picture. {0}".format(tdata_jpg))
+#
+#    reg_str_gif = 'comment-(\d*?)">\d*?</a></span><p>.*?\s*<img src="[a-zA-z]+://[^\s]+?" org_src="([a-zA-z]+://[^\s]+?)" o'
+#    pattern_gif = re.compile(reg_str_gif)
+#    result_gif = pattern_gif.findall(pic_body)
+#    for tdata_gif in result_gif:
+#        if not tdata_gif[0] in hash_pic_message:
+#            hash_pic_message[tdata_gif[0]]={}
+#            hash_pic_message[tdata_gif[0]]['URL']=tdata_gif[1]
+#            hash_pic_message[tdata_gif[0]]['FileName']=get_file_name2(tdata_gif[1])
+#            #print(tdata_gif)
+#        else:
+#            print("...error... already has this picture. {0}".format(tdata_gif))
+#
+#    reg_str_vote = '<div class="vote" id="vote-(\d*?)">.*?<span id="cos_support-\d*?">(\d*?)<\/span>.*?<span id="cos_unsupport-\d*?">(\d*?)<\/span>'
+#    pattern_vote = re.compile(reg_str_vote)
+#    result_vote = pattern_vote.findall(pic_body)
+#    for tdata_vote in result_vote:
+#        if tdata_vote[0] in hash_pic_message:
+#            support = tdata_vote[1]
+#            unsupport = tdata_vote[2]
+#            scale =  float(support) / float(unsupport)
+#            rank = get_scale(scale)
+#            hash_pic_message[tdata_vote[0]]["Support"] = support
+#            hash_pic_message[tdata_vote[0]]["UnSupport"] = unsupport
+#            hash_pic_message[tdata_vote[0]]["Scale"] = scale
+#            hash_pic_message[tdata_vote[0]]["Rank"] = rank
+#        else:
+#            print("...error... no already has this picture. {0}".format(tdata_vote))
+#
+#    for value in hash_pic_message.values():
+#        #print(value)
+#        pass
+#    return hash_pic_message.values()
+##################################################################
+
 
 if __name__=="__main__":
     #reading config.json file
@@ -136,7 +200,9 @@ if __name__=="__main__":
     for page in range(int(startpage),int(endpage)):
         for url_index in get_jiandan_mm_pic(page):
             #print (url_index,"\n")
-            get_jpg(url_index["URL"],url_index["Support"],url_index["UnSupport"])
-            time.sleep(0.1)
+#            print(url_index["URL"][0])
+            for url in url_index["URL"]:
+                get_jpg(url_index["ID"],url,url_index["Support"],url_index["UnSupport"])
+            time.sleep(0.2)
         time.sleep(0.8)
 
